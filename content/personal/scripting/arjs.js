33f335af4f77764f00a83fa3945a7a61a7e325f3
Jay Reese <reese.j.jordon@gmail.com>
2018-02-08 10:16:41 -0600

    changed to add document ready.

--- content/personal/scripting/arjs.js
+++ content/personal/scripting/arjs.js
@@ -2,15 +2,17 @@
  * Tabbed code created by Abstract Reality.
  * Able to be used just about anywhere on the site as a whole.
 **/
-$(".tb-b").click(function(){
-  if($(this).closest(".tb-a").hasClass("tb-t"))
-  {
-     $(".tb-t").children(".tb-c").css("display", "none").eq($(this).index()).css("display", "block");
-  }
-  else 
-  {
-       $(this).closest(".tb-a").siblings(".tb-a").children(".tb-c").css("display", "none").eq($(this).index()).css("display", "block");
-  }
+$(document).ready(function(){
+	$(".tb-b").click(function(){
+	  if($(this).closest(".tb-a").hasClass("tb-t"))
+	  {
+	     $(".tb-t").children(".tb-c").css("display", "none").eq($(this).index()).css("display", "block");
+	  }
+	  else 
+	  {
+	       $(this).closest(".tb-a").siblings(".tb-a").children(".tb-c").css("display", "none").eq($(this).index()).css("display", "block");
+	  }
+	});
 });
 
 /** 
@@ -18,20 +20,22 @@ $(".tb-b").click(function(){
  * Code made by Virendra.
  * Code adapted for use on AR.
  **/ 
-$(".tooltips div").hover(function(event){
-  var toolTip = $(this).attr('Tooltip');
-  $('<span class="c-tooltip"></span>').text(toolTip)
-        .appendTo('body')
-        .css('top', (event.pageY - 10) + 'px')
-        .css('left', (event.pageX + 20) + 'px')
-        .fadeIn('slow');
-  }, function() {
-    $('.c-tooltip').remove();
-}).mousemove(function(event) {
-    $('.c-tooltip')
-    .css('top', (event.pageY - 5) + 'px')
-    .css('left', (event.pageX + 20) + 'px');
-}); 
+$(document).ready(function(){
+	$(".tooltips div").hover(function(event){
+	  var toolTip = $(this).attr('Tooltip');
+	  $('<span class="c-tooltip"></span>').text(toolTip)
+	        .appendTo('body')
+	        .css('top', (event.pageY - 10) + 'px')
+	        .css('left', (event.pageX + 20) + 'px')
+	        .fadeIn('slow');
+	  }, function() {
+	    $('.c-tooltip').remove();
+	}).mousemove(function(event) {
+	    $('.c-tooltip')
+	    .css('top', (event.pageY - 5) + 'px')
+	    .css('left', (event.pageX + 20) + 'px');
+	});
+});
 
 
 
@@ -40,17 +44,19 @@ $(".tooltips div").hover(function(event){
 **/
 
 // Sanctuary button clicker code.
-$(".sanc-info .tb-b").click(function(){
-  if(!$(this).hasClass("chosen-tab"))
-  {
-      $(this).closest(".tb-a").siblings(".sanc-body").children(".sanc-temp-default").css("display", "none");
-      $(this).addClass("chosen-tab").siblings().removeClass("chosen-tab");
-  }
-  else
-  {
-      if($(this).closest(".tb-a").hasClass("tb-t")) {
-        $(this).removeClass("chosen-tab").closest(".tb-a").siblings(".sanc-body").children(".tb-c").css("display", "none");
-        $(this).closest(".tb-a").siblings(".sanc-body").children(".sanc-temp-default").css("display", "block");
-      }
-  }
+$(document).ready(function(){
+	$(".sanc-info .tb-b").click(function(){
+	  if(!$(this).hasClass("chosen-tab"))
+	  {
+	      $(this).closest(".tb-a").siblings(".sanc-body").children(".sanc-temp-default").css("display", "none");
+	      $(this).addClass("chosen-tab").siblings().removeClass("chosen-tab");
+	  }
+	  else
+	  {
+	      if($(this).closest(".tb-a").hasClass("tb-t")) {
+	        $(this).removeClass("chosen-tab").closest(".tb-a").siblings(".sanc-body").children(".tb-c").css("display", "none");
+	        $(this).closest(".tb-a").siblings(".sanc-body").children(".sanc-temp-default").css("display", "block");
+	      }
+	  }
+	});
 });
